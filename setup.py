f2adffde8d6b81142a185bdf3259c51763979bfd
luismasuelli <luismasuelli@hotmail.com>
2015-04-22 22:10:15 -0500

    paquete de watches para los cantrips - renombramos el paquete

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='watch-cantrips',
+    name='python-watch-cantrips',
     version='0.0.1',
     namespace_packages=['cantrips'],
     packages=find_packages(),
