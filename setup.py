498ecd585e5e2593f4dcf70e80e54239b4314e2c
luismasuelli <luismasuelli@hotmail.com>
2017-10-12 11:16:13 -0500

    New version 0.0.2 with new requirerment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-watch-cantrips',
-    version='0.0.1',
+    version='0.0.2',
     namespace_packages=['cantrips'],
     packages=find_packages(),
     url='https://github.com/luismasuelli/python-watch-cantrips',
@@ -10,5 +10,5 @@ setup(
     author='Luis y Anita',
     author_email='luismasuelli@hotmail.com',
     description='Python watch cantrips. This will behave as AngularJS\'s cantrips',
-    install_requires=['python-cantrips>=0.6.6']
+    install_requires=['python-cantrips>=0.6.6', 'python-cantrips>=0.7.5']
 )
